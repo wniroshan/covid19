7d7a9de67f64c1b4c800724be95885e69a0e56f3
wniroshan <wniroshan@gmail.com>
2020-04-03 03:56:39 +1100

    Refactoring get_daily_deaths_by_country function

--- data.py
+++ data.py
@@ -2,14 +2,14 @@ import pandas as pd
 import re
 
 
-def get_daily_deaths_by_country(df):
+def get_daily_deaths_by_country(csv_as_df):
     """
-    Coverts the horizontally growing csv table to a vertically growing RDBMS friendly table with one 'date' column
-    :param df: DataFrame loaded from the csv file
+    Converts the horizontally growing csv table to a vertically growing RDBMS friendly table with one 'date' column
+    :param csv_as_df: DataFrame loaded from the csv file
     :return: DataFrame with one row per each Country and Date.
     """
 
-    csv_cols = df.columns.values
+    csv_cols = csv_as_df.columns.values
 
     # Regex selects the column names that start with the term 'Country'
     country_col = [col for col in csv_cols if re.match('[cC]ountry', col)]
@@ -21,11 +21,11 @@ def get_daily_deaths_by_country(df):
     # Regex selects the column names that look like a date
     date_cols = [col for col in csv_cols if re.match('(?:[0-9]{1,2}/){1,2}[0-9]{2}', col)]
 
-    # Use a common name for the country column and make a copy of the df.
-    df_by_country = df[country_col + date_cols].rename(columns={country_col[0]: 'country'})
+    stats_df = csv_as_df[country_col + date_cols]
+    stats_df = stats_df.rename(columns={country_col[0]: 'country'})  # use a short name for country
 
-    df_by_country = df_by_country.groupby('country', as_index=False).sum()
-    df_by_country = pd.melt(df_by_country, id_vars=['country'], var_name='date', value_name='deaths')
-    df_by_country['date'] = pd.to_datetime(df_by_country['date'], infer_datetime_format=True)
+    stats_df = stats_df.groupby('country', as_index=False).sum()  # sum of deaths of all states in a country
+    stats_df = pd.melt(stats_df, id_vars=['country'], var_name='date', value_name='deaths')  # arrange vertically
+    stats_df['date'] = pd.to_datetime(stats_df['date'], infer_datetime_format=True)
 
-    return df_by_country
+    return stats_df
