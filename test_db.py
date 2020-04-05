1e6793480692ab3bbb51e5c68ce65ee2178a4286
wniroshan <wniroshan@gmail.com>
2020-04-05 19:31:38 +1000

    Update unit tests to match refactored 'add_new_death_data' method

--- test_db.py
+++ test_db.py
@@ -31,39 +31,44 @@ class TestDatabase(unittest.TestCase):
         self.assertIsNotNone(con, "Database connection")
 
     def test_no_data_duplicates(self):
+        self.db.create_total_deaths_table()
+
         with self.db._create_connection() as con:
             cursor = con.cursor()
-            self.db.add_new_death_data(self.total_deaths_df, self.total_deaths_table)
+            self.db._insert_deaths_data(self.total_deaths_df, self.total_deaths_table)
             cursor.execute('SELECT COUNT(*) FROM ' + self.total_deaths_table + ';')
             row_count = cursor.fetchone()[0]
 
             # Try to add the same data again.
-            self.db.add_new_death_data(self.total_deaths_df, self.total_deaths_table)
+            self.db._insert_deaths_data(self.total_deaths_df, self.total_deaths_table)
             cursor.execute('SELECT COUNT(*) FROM ' + self.total_deaths_table + ';')
             new_row_count = cursor.fetchone()[0]
 
         self.assertEqual(row_count, new_row_count)
 
     def test_function_output_and_table_rows(self):
+        self.db.create_total_deaths_table()
         with self.db._create_connection() as con:
             cursor = con.cursor()
-            total_rows, new_rows = self.db.add_new_death_data(self.total_deaths_df, self.total_deaths_table)
+            total_rows, new_rows = self.db._insert_deaths_data(self.total_deaths_df, self.total_deaths_table)
             cursor.execute('SELECT COUNT(*) FROM ' + self.total_deaths_table + ';')
             row_count = cursor.fetchone()[0]
         self.assertEqual(total_rows, row_count, "Number of rows in table needs to match add_new_death_data function "
                                                 "total rows output")
 
     def test_adding_new_rows_to_table(self):
+        self.db.create_total_deaths_table()
+
         # Create a new df by removing some data
         starting_df = self.total_deaths_df[self.total_deaths_df['date'] != pd.to_datetime('2/13/2020')]
-        self.db.add_new_death_data(starting_df,
-                                   self.total_deaths_table)  # Add it to the table. This is the starting state.
+        self.db._insert_deaths_data(starting_df, self.total_deaths_table)  # Add it to the table. This is the
+        # starting state.
 
         # Now we mimic a new update
         new_data_df = self.total_deaths_df  # Df has extra rows with latest stats
         new_data_df.at[1, 'deaths'] = 1  # A retrospective update of the data
 
-        self.db.add_new_death_data(new_data_df, self.total_deaths_table)
+        self.db._insert_deaths_data(new_data_df, self.total_deaths_table)
 
         with self.db._create_connection() as con:
             cursor = con.cursor()
